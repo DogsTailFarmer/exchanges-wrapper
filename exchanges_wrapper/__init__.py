5feb95f59ac031e1d241dcfe71195a441901dab4
DogsTaiFarmer <jerry.fedorenko@yahoo.com>
2023-06-21 14:56:09 +0300

    1.3.1

--- exchanges_wrapper/__init__.py
+++ exchanges_wrapper/__init__.py
@@ -12,7 +12,7 @@ __maintainer__ = "Jerry Fedorenko"
 __contact__ = "https://github.com/DogsTailFarmer"
 __email__ = "jerry.fedorenko@yahoo.com"
 __credits__ = ["https://github.com/DanyaSWorlD"]
-__version__ = "1.3.1b0"
+__version__ = "1.3.1"
 
 from pathlib import Path
 import shutil
